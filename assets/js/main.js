e3c4425c50745bb2d49d73db1969e0e4b639163d
Fabrizio1919 <fabriziosalerno19@gmail.com>
2023-03-07 16:24:08 +0100

    Fix in js remove let add const - add aseggno innerHTML a sentence

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,24 @@
 // Chiedere all'utente il suo nome
-let userName = prompt ("inserisci il tuo nome");
+const userName = prompt ("inserisci il tuo nome");
 console.log(userName);
 
 // Chiedere all'utente il suo cognome 
-let userSurname = prompt ("inserisci il tuo cognome");
+const userSurname = prompt ("inserisci il tuo cognome");
 console.log(userSurname);
 
 // Chiedere all'utente il suo colore preferito 
-let userColor = prompt ("inserisci il tuo colore preferito");
+const userColor = prompt ("inserisci il tuo colore preferito");
 console.log(userColor);
 
 // Aggiungere numero 23
-let number = '23'
+const number = '23';
+
+const html = 
+document.getElementById('utente');
 
 // Aggiungere Nome, Cognome, Colore preferito e numero 23 tutt0 attaccat0
-let sentence = `${userName}${userSurname}${userColor}${number}`
-console.log(sentence)
+let sentence = `${userName}${userSurname}${userColor}${number}`;
+console.log(sentence);
+
+// Assegno l'innerHTMML a sentence
+html.innerHTML = sentence;
