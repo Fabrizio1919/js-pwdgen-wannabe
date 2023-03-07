6f9f82407f118d68c5ab6448b4b50e36e3d8e2ef
Fabrizio1919 <fabriziosalerno19@gmail.com>
2023-03-07 16:58:49 +0100

    Fix in js space in prompt and remove html in row 17 add utenteData

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,25 @@
 // Chiedere all'utente il suo nome
-const userName = prompt ("inserisci il tuo nome");
+const userName = prompt("inserisci il tuo nome");
 console.log(userName);
 
 // Chiedere all'utente il suo cognome 
-const userSurname = prompt ("inserisci il tuo cognome");
+const userSurname = prompt("inserisci il tuo cognome");
 console.log(userSurname);
 
 // Chiedere all'utente il suo colore preferito 
-const userColor = prompt ("inserisci il tuo colore preferito");
+const userColor = prompt("inserisci il tuo colore preferito");
 console.log(userColor);
 
 // Aggiungere numero 23
 const number = '23';
 
-const html = 
+// Aggiungo ID in js
+const utenteData = 
 document.getElementById('utente');
 
 // Aggiungere Nome, Cognome, Colore preferito e numero 23 tutt0 attaccat0
-let sentence = `${userName}${userSurname}${userColor}${number}`;
+const sentence =`${userName}${userSurname}${userColor}${number}`;
 console.log(sentence);
 
 // Assegno l'innerHTMML a sentence
-html.innerHTML = sentence;
+utenteData.innerHTML = sentence;
