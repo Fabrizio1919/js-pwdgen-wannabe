94999a1384ea507974bf7fe75ee9bed783e0a358
Fabrizio1919 <fabriziosalerno19@gmail.com>
2023-03-07 15:05:30 +0100

    STRUCTURE Html Css and in JS,+ add in js user data

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+// Chiedere all'utente il suo nome
+let userName = prompt ("inserisci il tuo nome");
+console.log(userName);
+
+// Chiedere all'utente il suo cognome 
+let userSurname = prompt ("inserisci il tuo cognome");
+console.log(userSurname);
+
+// Chiedere all'utente il suo colore preferito 
+let userColor = prompt ("inserisci il tuo colore preferito");
+console.log(userColor);
+
+// Aggiungere numero 23
+let number = '23'
+
+// Aggiungere Nome, Cognome, Colore preferito e numero 23 tutt0 attaccat0
+let sentence = `${userName}${userSurname}${userColor}${number}`
+console.log(sentence)
